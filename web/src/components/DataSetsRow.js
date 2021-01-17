63805c09b8e2f4c2b81312afb8a8498e10c6c4bd
Brian Hoffman <brianchoffman@gmail.com>
2021-01-17 12:51:03 -0500

    Fixing css

--- web/src/components/DataSetsRow.js
+++ web/src/components/DataSetsRow.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from "react-router-dom";
+import {getAxiosInstance} from "./Auth";
 
 function DataSetsRow(props) {
     const imageFilename = props.metadata.imageFilename;
     const title = props.metadata.title;
     const shortDescription = props.metadata.shortDescription;
 
+    const datasetImage = "/api/v1/image/" + imageFilename;
 
     return (
         <div class="datasets-row">
-            {imageFilename} {title}<br/>
-            {shortDescription}
+            <div><img className="datasets-image" src={datasetImage}/></div>
+            <div>
+                {title}<br/>
+                {shortDescription}
+            </div>
+            <div class="datasets-details">
+                <div><button>Details</button></div>
+                <div>1.23 FIL</div>
+            </div>
         </div>
     )
 }
