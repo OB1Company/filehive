d7c7d4d043436b1c44b48cbed2814c59ea7e0461
Brian Hoffman <brianchoffman@gmail.com>
2021-01-29 20:31:07 -0500

    Add Edit/Create Switch

--- web/src/pages/CreatePage.js
+++ web/src/pages/CreatePage.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import Header from '../Header'
 import Footer from '../Footer'
 import Create from '../components/Create.js'
+import Edit from '../components/Edit.js'
 
-export default function LoginPage() {
-  return (
+export default function CreatePage() {
+
+    let { id } = useParams();
+
+    return (
     <div className="container">
       <Header/>
       <div className="maincontent">
-        <Create/>
+          {id === undefined
+              ? <Create/>
+              : <Edit/>
+          }
       </div>
       <Footer/>
     </div>
-  )
+    )
 }
