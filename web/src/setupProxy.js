6549733311d280b21200dd2b37eb11668a9c089f
Brian Hoffman <brianchoffman@gmail.com>
2021-02-02 21:34:16 -0500

    Move to dotenv

--- web/src/setupProxy.js
+++ web/src/setupProxy.js
@@ -1,19 +1,21 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+require('dotenv').config()
+
 module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:8080',
-            changeOrigin: true,
-            cookieDomainRewrite: "localhost",
-            cookiePathRewrite: "/",
+            target: process.env.REACT_APP_TARGET_SERVER,
+            changeOrigin: process.env.REACT_APP_CHANGE_ORIGIN,
+            cookieDomainRewrite: process.env.REACT_APP_COOKIE_DOMAIN,
+            cookiePathRewrite: process.env.REACT_APP_COOKIE_PATH,
             debug: true,
             onProxyReq: function (proxyReq, req, res) {
-                // console.log(req.headers.cookie);
+                // console.log(req.headers['cookies']);
             },
             onProxyRes: function (proxyRes, req, res) {
-                // console.log(req.headers.cookie);
+                // console.log(proxyRes.headers);
             },
         })
     );
