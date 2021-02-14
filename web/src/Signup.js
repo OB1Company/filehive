fb893b3846fc7a7e41359b6003ede7ff44bfeda4
Brian Hoffman <brianchoffman@gmail.com>
2021-02-14 15:26:35 -0500

    Add loading buttons

--- web/src/Signup.js
+++ web/src/Signup.js
@@ -29,18 +29,27 @@ function Signup() {
 
     if(name === "") {
       setError("Name is required");
+      setIsRegistering(false);
       return false;
     }
     if(country === "") {
       setError("Country is required")
+      setIsRegistering(false);
       return false;
     }
 
     const instance = getAxiosInstance();
 
-    const createUserUrl = "/api/v1/user";
+    const url = "/api/v1/user";
+
+    console.log(url);
+    await instance.get(url)
+        .catch((err)=>{
+          console.debug(err);
+        }); // Refresh CSRF
+
     await instance.post(
-        createUserUrl,
+        url,
         data
     ).then((data) => {
       // Successful login
@@ -52,12 +61,16 @@ function Signup() {
       setIsRegistering(false);
       if(error.response.status === 409) {
         setError("This email address has already been used");
+        setIsRegistering(false);
         return false;
       }
       if(error.response.status === 400) {
         setError("There was an error with your registration: "+error.response.data.error);
+        setIsRegistering(false);
         return false;
       }
+      setError(error.response.data);
+      setIsRegistering(false);
       return false;
     });
 
