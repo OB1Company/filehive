f0a394b7e8b85257c37b521518877cb7c943f40c
Brian Hoffman <hoffmabc@users.noreply.github.com>
2021-02-15 08:06:28 -0500

    Ad en locale

--- web/src/components/dashboard/Wallet.js
+++ web/src/components/dashboard/Wallet.js
@@ -7,6 +7,7 @@ import {getAxiosInstance} from "../Auth";
 import { FilecoinPrice } from "../utilities/images";
 import DataSetsRow from "../DataSetsRow";
 import TimeAgo from 'javascript-time-ago';   
+import en from 'javascript-time-ago/locale/en';
 
 TimeAgo.addDefaultLocale(en);
 
